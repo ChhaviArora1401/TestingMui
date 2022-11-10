9f7756c7366d535b4009973037d5612cdfe39968
Chhavi Arora <chhaviarora1401@gmail.com>
2022-11-10 12:23:13 +0530

    Create App.js

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import "./styles.css";
+import Home from "./Home";
+
+export default function App() {
+  return (
+    <div className="App">
+      <h1>Testing MUI</h1>
+      <Home />
+    </div>
+  );
+}
